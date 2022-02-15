764f24802cca016c5c9410a2ec05b21108cd5875
Jason Grafmiller (English Language and Linguistics) <j.grafmiller@bham.ac.uk>
2022-02-15 11:51:33 +0000

    set col_types; add "sitten" to variants

--- scripts_R/custom_functions.R
+++ scripts_R/custom_functions.R
@@ -9,7 +9,7 @@
 #
 ReadDataset <- function(file, delim = "\t"){
   df <- vroom::vroom(file, delim = delim, escape_double = FALSE, trim_ws = TRUE,
-                   col_types = cols()) %>%
+                     col_types = "c") %>%
     janitor::clean_names() %>%
     dplyr::mutate(across(.cols = everything(), as.character))
 }
@@ -57,7 +57,7 @@ CodeCountry <- function(country) {
 
 # get the verb
 GetVerb <- function(token){
-  if(grepl("(sat|sitting)", token)){
+  if(grepl("(sat|sitting|sitten)", token)){
     v <- "sit"
   } else v <- "stand"
   return(v)
@@ -67,7 +67,7 @@ GetVerb <- function(token){
 GetVariant <- function(token){
   form <- stringr::str_split(token, " ", simplify = T) %>%
     last()
-  variant <- ifelse(form %in% c("sat", "stood"), "ed", "ing")
+  variant <- ifelse(form %in% c("sat", "stood", "sitten"), "ed", "ing")
   return(variant)
 }
 
@@ -171,6 +171,56 @@ GetTenseAspect <- function(token, before){
   }
 
   return(tense)
+}
+
+
+# functions for tables ----------------------------------------------------
+
+MakeCorpusTable <- function(df){
+  header <- c("", "sat", "sat", "sitting", "sitting",
+              "stood", "stood", "standing", "standing") %>%
+    as.list()
+  names(header) <- paste0("V",1:9)
+
+ df %>%
+  flextable() %>%
+    colformat_char(j = c(3,5,7,9), prefix = "(", suffix = "%)") %>%
+    set_header_labels(
+      values = header
+    ) %>%
+    merge_h(part = "header") %>%
+    italic(i = 1, j = 2:9, part = "header", italic = TRUE) %>%
+    width(j = c(3,5,7,9), width = .5) %>%
+    font(fontname = "Times New Roman", part = "all") %>%
+    fontsize(size = 12) %>%
+    fontsize(j = c(3,5,7,9), size = 10, part = "body") %>%
+    theme_booktabs() %>%
+    set_table_properties(width = 1, layout = "autofit") %>%
+    align(j = 2:9, align = "right", part = "body") %>%
+    align(j = 2:9, align = "center", part = "header") %>%
+    vline(j = 5)
+}
+
+MakeHistoricalTable <- function(df){
+  header <- c("", "Period", "sat", "sitting", "stood", "standing") %>%
+    as.list()
+
+  names(header) <- names(df)
+
+  df %>%
+    flextable() %>%
+    set_header_labels(
+      values = header
+    ) %>%
+    # merge_h(part = "header") %>%
+    italic(j = 3:6, part = "header", italic = TRUE) %>%
+    width(j = 2, width = .5) %>%
+    font(fontname = "Times New Roman", part = "all") %>%
+    fontsize(size = 12) %>%
+    theme_booktabs() %>%
+    set_table_properties(width = 1, layout = "autofit") %>%
+    align(j = 3:6, align = "right", part = "all") %>%
+    align(j = 1:2, align = "left", part = "all")
 }
 
 
