b8a1611722f540c3180c0f571e646765c8ab45bf
Jason Grafmiller (English Language and Linguistics) <j.grafmiller@bham.ac.uk>
2023-02-23 14:02:11 +0000

    add model for sex in BNC2014

--- R/model_functions.R
+++ R/model_functions.R
@@ -6,6 +6,18 @@
 # project
 # -------------------------------------------------------------------------
 
+FitModelSexBNC <- function(data){
+  data <- data %>%
+    dplyr::filter(postmodifier_vp == "y") %>%
+    mutate(variant = as.factor(variant) %>%
+             relevel(ref = "ing"),
+           dist_to_post_vp = as.integer(dist_to_post_vp) %>%
+             as.factor())
+  
+  m <- glm(variant ~ verb * (dist_to_post_vp + subj_person),
+           data = data, family = binomial)
+}
+
 FitModelDistVP <- function(data){
   data <- data %>%
     dplyr::filter(postmodifier_vp == "y") %>%
@@ -19,4 +31,3 @@ FitModelDistVP <- function(data){
 
   return(m)
 }
-
